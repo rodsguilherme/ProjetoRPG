3a80360bc09a51f01eb510e75d49b6a5b9304a8f
Guilherme <rodsguilherme@gmail.com>
2019-08-26 11:50:01 -0300

    Base para o projeto

---
+++ Api/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// =============== Importação dos controllers ====================
+
+
+
+
+
+// ===============================================================
+
+app.get('/', function (req, res) {
+  res.send('Projeto de RPG');
+});
+
+const porta = 3000;
+app.listen(porta, ()  => {
+  console.log(`Rodando na porta ${porta}!`);
+})
+
+
