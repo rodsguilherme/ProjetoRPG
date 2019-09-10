6495b3e0a74b13e39547715e04912cc99cfa6d75
Your Name <rodsguilherme//@gmail.com>
2019-09-10 10:55:24 -0300

    Adiciona o Banco

--- Api/Controllers/UserController.js
+++ Api/Controllers/UserController.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const router = express.Router();
+
+const database = require('../database/bancoDeDados.js');
+
+router.get('./', (request, response) => {
+    if (request.body.nome && request.body.email && request.body.senha) {
+        let select = sqlite.run(`SELECT nome from Usuario where nome = Upper(${request.body.nome})`);
+        response.status(200).send(select);
+
+    }
+    else
+        response.status(404).send();
+})
