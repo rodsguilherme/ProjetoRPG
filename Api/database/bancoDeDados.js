327ef27f9cd998da4b20a332f68bee6941ddf77d
Guilherme <rodsguilherme@gmail.com>
2019-09-11 16:24:22 -0300

    Add Banco de dados 3.0

--- Api/database/bancoDeDados.js
+++
@@ -1,9 +0,0 @@
-var sqlite = require('sqlite-sync')
-
-//Conectando
-sqlite.connect('Rpg.sql')
-
-//Criando exemplo de tabela 
-// sqlite.run('Create Table Comida(ID INTEGER PRIMARY KEY AUTOINCREMENT')
-
-module.exports = sqlite
--- Api/server.js
+++ Api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const database = require('./database/ConnectDB')
 
 const app = express();
 app.use(cors());
