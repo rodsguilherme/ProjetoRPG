e5cb2ba4f4a29a544826a851c53deb469290a548
Your Name <rodsguilherme//@gmail.com>
2019-11-26 10:08:59 -0300

    hotfix card persistent

--- app/src/components/inventory.vue
+++ app/src/components/inventory.vue
@@ -35,95 +35,93 @@
                   :loading="loading && i==index"
                   class="white--text"
                   color="purple darken-2"
-                  @click="showDetails(card); index = i"
+                  @click="showDetails(card); index = i; dialog =true"
                 >Details</v-btn>
               </v-col>
             </v-row>
           </v-card-actions>
         </v-card>
-       
-          <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
-            <v-card dark>
-              <v-toolbar class="deep-purple lighten-1">
-                <v-btn icon dark @click="dialog = false">
-                  <v-icon>mdi-close</v-icon>
-                </v-btn>
-                <v-row align="center" justify="center">
-                  <v-toolbar-title class="text-center">Card</v-toolbar-title>
-                </v-row>
-              </v-toolbar>
-              <v-row justify="center" align="center" class="mt-5">
-                <v-card elevation="20" max-width="500vw" width="75vw" max-height="85vh">
-                  <v-row class="mx-auto">
-                    <v-row class="mx-auto">
-                      <v-col lg="6">
-                        <h1 class="title">Attributes</h1>
-                        <v-divider inset></v-divider>
-                        <v-row>
-                          <v-text-field class="pr-3" value="Charisma" solo readonly></v-text-field>
-                          <v-text-field :value="cardSelected.charisma" solo readonly></v-text-field>
-                        </v-row>
-                        <v-row>
-                          <v-text-field class="pr-3" value="Constitution" solo readonly></v-text-field>
-                          <v-text-field :value="cardSelected.constitution" solo readonly></v-text-field>
-                        </v-row>
-                        <v-row>
-                          <v-text-field class="pr-3" value="Dexterity" solo readonly></v-text-field>
-                          <v-text-field :value="cardSelected.dexterity" solo readonly></v-text-field>
-                        </v-row>
-                        <v-row>
-                          <v-text-field class="pr-3" value="Intelligence" solo readonly></v-text-field>
-                          <v-text-field :value="cardSelected.intelligence" solo readonly></v-text-field>
-                        </v-row>
-                        <v-row>
-                          <v-text-field class="pr-3" value="Strength" solo readonly></v-text-field>
-                          <v-text-field :value="cardSelected.strength" solo readonly></v-text-field>
-                        </v-row>
-                        <v-row>
-                          <v-text-field class="pr-3" value="Winsdow" solo readonly></v-text-field>
-                          <v-text-field :value="cardSelected.winsdow" solo readonly></v-text-field>
-                        </v-row>
-                      </v-col>
-                      <div class="mx-2"></div>
-                      <v-row class="mx-auto">
-                        <v-col lg="12">
-                          <h1 class="title">Data</h1>
-                          <v-divider inset></v-divider>
-                          <v-row>
-                            <v-text-field class="pr-3" dense value="Name" solo readonly></v-text-field>
-                            <v-text-field value="Nome do jogador" solo dense readonly></v-text-field>
-                          </v-row>
-                          <v-row>
-                            <v-text-field class="pr-3" value="Persona" dense solo readonly></v-text-field>
-                            <v-text-field :value="cardSelected.name" solo dense readonly></v-text-field>
-                          </v-row>
-                          <v-row>
-                            <v-text-field class="pr-3" value="Alignment" dense solo readonly></v-text-field>
-                            <v-text-field :value="cardSelected.alignment" dense solo readonly></v-text-field>
-                          </v-row>
-                          <v-row>
-                            <v-text-field class="pr-3" value="Race" solo dense readonly></v-text-field>
-                            <v-text-field :value="cardSelected.races" solo dense readonly></v-text-field>
-                          </v-row>
-                          <v-row>
-                            <v-text-field class="pr-3" value="Kind" solo dense readonly></v-text-field>
-                            <v-text-field :value="cardSelected.kinds" solo dense readonly></v-text-field>
-                          </v-row>
-                          <v-row>
-                            <v-text-field class="pr-3" value="Health Points" solo dense readonly></v-text-field>
-                            <v-text-field :value="cardSelected.hp" solo dense readonly></v-text-field>
-                          </v-row>
-                        </v-col>
-                      </v-row>
-                    </v-row>
-                  </v-row>
-                </v-card>
-              </v-row>
-            </v-card>
-          </v-dialog>
-    
       </v-col>
     </v-row>
+    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <v-card dark>
+        <v-toolbar class="deep-purple lighten-1">
+          <v-btn icon dark @click="dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <v-row align="center" justify="center">
+            <v-toolbar-title class="text-center">Card</v-toolbar-title>
+          </v-row>
+        </v-toolbar>
+        <v-row justify="center" align="center" class="pt-12">
+          <v-card elevation="20" max-width="500vw" width="80vw" max-height="85vh">
+            <v-row class="mx-6">
+              <v-row class="mx-auto">
+                <v-col lg="6">
+                  <h1 class="title">Attributes</h1>
+                  <v-divider inset></v-divider>
+                  <v-row>
+                    <v-text-field class="pr-3" value="Charisma" solo readonly></v-text-field>
+                    <v-text-field :value="cardSelected.charisma" solo readonly></v-text-field>
+                  </v-row>
+                  <v-row>
+                    <v-text-field class="pr-3" value="Constitution" solo readonly></v-text-field>
+                    <v-text-field :value="cardSelected.constitution" solo readonly></v-text-field>
+                  </v-row>
+                  <v-row>
+                    <v-text-field class="pr-3" value="Dexterity" solo readonly></v-text-field>
+                    <v-text-field :value="cardSelected.dexterity" solo readonly></v-text-field>
+                  </v-row>
+                  <v-row>
+                    <v-text-field class="pr-3" value="Intelligence" solo readonly></v-text-field>
+                    <v-text-field :value="cardSelected.intelligence" solo readonly></v-text-field>
+                  </v-row>
+                  <v-row>
+                    <v-text-field class="pr-3" value="Strength" solo readonly></v-text-field>
+                    <v-text-field :value="cardSelected.strength" solo readonly></v-text-field>
+                  </v-row>
+                  <v-row>
+                    <v-text-field class="pr-3" value="Winsdow" solo readonly></v-text-field>
+                    <v-text-field :value="cardSelected.winsdow" solo readonly></v-text-field>
+                  </v-row>
+                </v-col>
+                <v-divider vertical class="mx-3"></v-divider>
+                <v-row class="mx-auto">
+                  <v-col lg="12">
+                    <h1 class="title">Data</h1>
+                    <v-divider inset></v-divider>
+                    <v-row>
+                      <v-text-field class="pr-3" value="Name" solo readonly></v-text-field>
+                      <v-text-field value="Nome do jogador" solo readonly></v-text-field>
+                    </v-row>
+                    <v-row>
+                      <v-text-field class="pr-3" value="Persona" solo readonly></v-text-field>
+                      <v-text-field :value="cardSelected.name" solo readonly></v-text-field>
+                    </v-row>
+                    <v-row>
+                      <v-text-field class="pr-3" value="Alignment" solo readonly></v-text-field>
+                      <v-text-field :value="cardSelected.alignment" solo readonly></v-text-field>
+                    </v-row>
+                    <v-row>
+                      <v-text-field class="pr-3" value="Race" solo readonly></v-text-field>
+                      <v-text-field :value="cardSelected.races" solo readonly></v-text-field>
+                    </v-row>
+                    <v-row>
+                      <v-text-field class="pr-3" value="Kind" solo readonly></v-text-field>
+                      <v-text-field :value="cardSelected.kinds" solo readonly></v-text-field>
+                    </v-row>
+                    <v-row>
+                      <v-text-field class="pr-3" value="Health Points" solo readonly></v-text-field>
+                      <v-text-field :value="cardSelected.hp" solo readonly></v-text-field>
+                    </v-row>
+                  </v-col>
+                </v-row>
+              </v-row>
+            </v-row>
+          </v-card>
+        </v-row>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
