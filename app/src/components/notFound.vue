97d18c93b56994b0eec32f6ef5a7f0980e75f786
GuilhermeRods <rodsguilherme//@gmail.com>
2019-12-02 11:29:15 -0300

    muda card de quando user n tem cards

--- app/src/components/notFound.vue
+++ app/src/components/notFound.vue
@@ -1,19 +1,12 @@
 <template>
   <v-container fluid>
-      <v-card  min-height="20vh" width="61vw" class="mx-auto" dark>
-        <v-row  class="mx-auto pt-12 mt-12" dense>
-          <v-card-title
-            class="display-1 font-weight-bold deep-purple--text text--lighten-2"
-          >Opa, parece que você ainda não tem cards salvos :(</v-card-title>
-        </v-row>
-       
-        <v-row class="mx-auto">
-           <v-spacer></v-spacer>
-      <v-card-actions >
-        <v-btn  text class="deep-purple lighten-2" to="/">Create</v-btn>
-      </v-card-actions>
-        </v-row>
-      </v-card>
+    <v-card min-height="10vh" width="100vw" dark>
+      <v-row class="mx-auto ml-10">
+        <v-card-title
+          class="display-1 font-weight-bold deep-purple--text text--lighten-2"
+        >Opa, parece que você ainda não tem cards salvos :(</v-card-title>
+      </v-row>
+    </v-card>
   </v-container>
 </template>
 
